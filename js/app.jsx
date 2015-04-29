89869e818523fd7dac9d6778efafa7905a3828a2
Phoebe Hong <phoebejhhong@gmail.com>
2015-04-29 13:31:14 +0900

    set up for static version in react

--- js/app.jsx
+++ js/app.jsx
@@ -1,18 +1,49 @@
+var data = [
+  {
+    "text": "drink", "completed": "false"
+  },
+  {
+    "text": "eat", "completed": "true"
+  }
+];
+
 var app = app || {};
 
 (function () {
-  var todoApp = app.todoApp = React.createClass({
+  var TodoApp = React.createClass({
     render: function() {
       return (
         <div className="todoApp">
-          I am todoApp
+          <input className="newTodo" type="text"
+            placeholder="What needs to be done?"/>
+          <TodoList />
         </div>
       );
     },
   });
 
+  var TodoList = React.createClass({
+    render: function() {
+      return (
+        <ul className="todoList">
+          
+        </ul>
+      );
+    },
+  });
+
+  var TodoItem = React.createClass({
+    render: function() {
+      return (
+        <li className="todoItem">
+          <p>I am todoItem</p>
+        </li>
+      );
+    },
+  });
+
   React.render(
-    <todoApp />,
+    <TodoApp data="data" />,
     document.getElementById('todoApp')
   );
 
