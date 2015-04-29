0161fa697bb3fca7ee575a6155430c09ca561c85
Phoebe Hong <phoebejhhong@gmail.com>
2015-04-29 11:18:25 +0900

    more setup but had to stop because JSX transformer sends ajax request

---
+++ js/app.jsx
@@ -0,0 +1,20 @@
+var app = app || {};
+
+(function () {
+
+  var todoApp = app.todoApp = React.createClass({
+    render: function() {
+      return (
+        <div className="todoApp">
+          I am todoApp
+        </div>
+      );
+    },
+  });
+
+  React.render(
+    <todoApp />,
+    document.getElementById('todoApp')
+  );
+
+})();
