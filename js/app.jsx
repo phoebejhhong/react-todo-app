f4df318a7e3005bf84f6c219c38be1c75deb8eec
Phoebe Hong <phoebejhhong@gmail.com>
2015-04-30 16:07:13 +0900

    ajax request to get todo.json and handel newTodo submit

--- js/app.jsx
+++ js/app.jsx
@@ -1,28 +1,57 @@
-var data = [
-  {
-    "text": "drink", "completed": "false"
-  },
-  {
-    "text": "eat", "completed": "true"
-  }
-];
-
 var app = app || {};
 
 (function () {
+  var TodoFooter = app.TodoFooter;
+
   var TodoApp = React.createClass({
+    getInitialState: function () {
+      return {todos: []};
+    },
+    componentDidMount: function () {
+      $.ajax({
+        url: this.props.url,
+        dataType: 'json',
+        success: function (todos) {
+          this.setState({todos: todos});
+        }.bind(this),
+        error: function (xhr, status, err) {
+          console.error(this.props.url, status, err.toString());
+        }.bind(this),
+      });
+    },
+    handleNewTodo: function (e) {
+      e.preventDefault();
+      var newTodoText = React.findDOMNode(this.refs.text).value.trim();
+      if (!newTodoText) {
+        return;
+      };
+      React.findDOMNode(this.refs.text).value= '';
+
+      var oldTodos = this.state.todos
+      var newTodos = oldTodos.concat([{
+        "text": newTodoText,
+        "completed": "false"
+      }]);
+
+      this.setState({todos: newTodos});
+    },
     render: function() {
       return (
         <div id="todoapp">
           <header id="header">
             <h1>todos</h1>
-            <input id="new-todo" type="text"
-              placeholder="What needs to be done?"/>
+            <form onSubmit={this.handleNewTodo}>
+              <input id="new-todo" type="text" ref="text"
+                placeholder="What needs to be done?"
+                />
+              <input type="submit" value="Post" />
+            </form>
           </header>
           <section id="main">
             <input id="toggle-all" type="checkbox"></input>
-            <TodoList data={this.props.data} />
+            <TodoList todos={this.state.todos} />
           </section>
+          <TodoFooter />
         </div>
       );
     },
@@ -30,7 +59,7 @@ var app = app || {};
 
   var TodoList = React.createClass({
     render: function() {
-      var todos = this.props.data.map(function(todo) {
+      var todos = this.props.todos.map(function(todo) {
         return (
           <TodoItem
             todo={todo}
@@ -63,7 +92,7 @@ var app = app || {};
   });
 
   React.render(
-    <TodoApp data={data} />,
+    <TodoApp url="todos.json" />,
     document.getElementById('todoApp')
   );
 
